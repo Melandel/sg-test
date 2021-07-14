d4162ab138834b6509d54d726ec8d6b22088d5a0
Minh-Tam TRAN <c_mtran@capsuletech.com>
2021-07-14 17:35:45 +0200

    feature  (Domain.RoomBooking): add property "Name"

--- Domain/RoomBooking/Room.cs
+++ Domain/RoomBooking/Room.cs
@@ -8,33 +8,46 @@ namespace Domain.RoomBooking
 	public class Room
 	{
 		public Guid id { get; }
-		private Room(Guid id) {
+		public string Name { get; }
+		private Room(Guid id, string name) {
 			this.id = id;
+			Name = name;
 		}
 
-		public static Room Create() {
+		public static Room CreateFromName(string name) {
 			var id = Guid.NewGuid();
-			return new Room(id);
+			return new Room(id, name);
 		}
-		public static Room Reconstitute(Guid id) {
-			return new Room(id);
+		public static Room Reconstitute(Guid id, string name) {
+			return new Room(id, name);
 		}
 
 #if COMPILE_TESTS
 		public class Should
 		{
 			[Fact]
-			public void Create_instance_without_throwing() {
-				var sut = Room.Create();
+			public void Create_instance_with_correct_properties() {
+				// Arrange
+				var name = "Room0";
+
+				// Act
+				var sut = Room.CreateFromName(name);
+
+				// Assert
+				Assert.Equal("Room0", sut.Name);
 			}
 
 			[Fact]
-			public void Reconstitute_instance_without_throwing() {
+			public void Reconstitute_instance_withcorrect_properties() {
 				// Arrange
 				var id = Guid.NewGuid();
+				var name = "Room0";
 
 				// Act & Assert
-				var sut = Room.Reconstitute(id);
+				var sut = Room.Reconstitute(id, name);
+
+				// Assert
+				Assert.Equal("Room0", sut.Name);
 			}
 		}
 #endif
